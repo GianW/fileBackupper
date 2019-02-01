2a541fb0905013257ea51e92138ccc8a930bf827
GianW <gian.cb@hotmail.com>
2019-02-01 18:14:03 -0200

    ajustes gerais

--- app/config.js
+++ app/config.js
@@ -2,9 +2,9 @@
 const fn = require('./functions.js');
 
 exports.set = function(config){
-  return fn.writeFile('./app/config.json', JSON.stringify(config));
+  return fn.writeFile('./app/data/config.json', JSON.stringify(config));
 };
 
 exports.get = function(){
-  return fn.readFile('./app/config.json');
+  return fn.readFile('./app/data/config.json');
 };
--- open.sh
+++ open.sh
@@ -1,2 +1,1 @@
-IF exist "node_modules"( echo "node_modules")
 node app
